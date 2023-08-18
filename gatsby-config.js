98e183d774e780494076598bc7d14c02692d3068
Christoffer Rydeståhl <kristoffer@oholics.com>
2023-08-18 16:36:03 +0200

    Removed fb connection

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,16 +30,5 @@ module.exports = {
         hashtags: false
       },
     },
-    {
-      resolve: `gatsby-source-facebook`,
-      options: {
-        places: [`${facebookPageID}`], // Can be either a numeric ID or the URL ID
-        params: {
-          fields: 'hours, posts { message, created_time }', // See Facebooks API to see what you can query for
-        },
-        key: process.env.FACEBOOK_GRAPH_TOKEN, // You will need to create a Facebook application and go through review in order to get an API token.
-        version: '5.0', // The version of the graph API to use. Defaults to 5.0
-      },
-    },
   ],
 };
